a31741eba3f3e9accdb611d601900c9b26e3b32c
Vũ Văn Nam <vuvannamnam1201@gmail.com>
2026-09-14 18:13:57 +0700

    build: verify optimized release configuration

--- PersonalFinance/app/build.gradle.kts
+++ PersonalFinance/app/build.gradle.kts
@@ -20,8 +20,12 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 
